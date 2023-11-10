d778a144ecf1fee09390494111137507c998d49a
yasitha@intervest.lk <yasitha@intervest.lk>
2023-11-10 11:47:55 +0530

    add comment

--- next-blog/lib/reqvest_data/manage_post.ts
+++ next-blog/lib/reqvest_data/manage_post.ts
@@ -1,7 +1,12 @@
 import { Post } from "@prisma/client";
 
 const getPosts = async (): Promise<Post> => {
+    //SSR
     const data = await fetch(`http://localhost:3000/api/article`,{ cache: 'no-store' });
+    //ISR
+    ///const data = await fetch(`http://localhost:3000/api/article`,{ next: { revalidate: 60 } });
+    //SSG
+    //const data = await fetch(`http://localhost:3000/api/article`,{ cache: 'force-cache' });
     const post = await data.json();
     return post.data;
 };
